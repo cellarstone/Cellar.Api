eb32a81f17fa7e8e832f118bb29d55c65a5be4ea
Ondřej Snop <giboncz@hotmail.cz>
2017-10-12 10:19:54 +0200

    ReceptionController API (not final)

--- Cellar.Api/Cellar.Api/Controllers/ReceptionController.cs
+++ Cellar.Api/Cellar.Api/Controllers/ReceptionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Cellar.Api.Models;
 
 namespace Cellar.Api.Controllers
 {
@@ -11,5 +12,48 @@ namespace Cellar.Api.Controllers
     [Route("api/Reception")]
     public class ReceptionController : ControllerBase
     {
+
+        [HttpGet("{roomId},{cleanType}")]
+        [Route("CallForClean/{roomId}/{cleanType}")]
+        public bool CallForClean(string roomId, string cleanType)
+        {
+            return true;
+        }
+
+        [HttpGet("{roomId}")]
+        [Route("CallForClean/{roomId}")]
+        public bool CallReception(string roomId)
+        {
+            return true;
+        }
+
+        [HttpPost("{roomId}")]
+        [Route("SomethingElse/{roomId}")]
+        public bool SomethingElse(string roomId, [FromBody]string value)
+        {
+            return true;
+        }
+
+        [HttpGet("{roomId}")]
+        [Route("GetSortiment/{roomId}")]
+        public object GetSortiment(string roomId)
+        {
+            return true;
+        }
+
+        [HttpPost("{roomId}")]
+        [Route("PlaceOrder/{roomId}")]
+        public bool PlaceOrder(string roomId, [FromBody]string value)
+        {
+            return true;
+        }
+
+        [HttpPost]
+        [Route("PlaceOrder")]
+        public bool PlaceOrder([FromBody]string value)
+        {
+            return true;
+        }
+
     }
 }
